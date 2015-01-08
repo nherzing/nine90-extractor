b6b88bd4446f79d2b24a702403252faa44314bdb
Nathan Herzing <nherzing@gmail.com>
2015-01-07 21:39:19 -0500

    Fix page 2 processing offsets

--- src/tax_extractor/core.clj
+++ src/tax_extractor/core.clj
@@ -8,32 +8,35 @@
            [java.io File PrintStream OutputStreamWriter FileOutputStream BufferedWriter]
            [org.apache.commons.io FilenameUtils]))
 
-(def row-height 225)
-(def padding 6)
+(def row-height 112)
+(def page-two-row-height 116)
+(def padding 3)
 (def on-page-one? (partial > 17))
-(def record-0-y-offset 3380)
-(def record-17-y-offset 1690)
+(def record-0-y-offset 1690)
+(def record-17-y-offset 845)
 (defn y-offset [idx]
-  (+ (if (on-page-one? idx)
-       record-0-y-offset
-       record-17-y-offset)
-     (* (mod idx 17) (+ row-height padding))))
+  (let [page-one? (on-page-one? idx)]
+    (+ (if page-one?
+         record-0-y-offset
+         record-17-y-offset)
+       (* (mod idx 17) (+ (if page-one? row-height page-two-row-height)
+                          padding)))))
 
 
 (defn name-rect [y-off]
-  (Rectangle. 30 y-off 1620 row-height))
+  (Rectangle. 15 y-off 810 row-height))
 
 (defn hours-rect [y-off]
-  (Rectangle. 1640 y-off 520 row-height))
+  (Rectangle. 820 y-off 260 row-height))
 
 (defn reportable-internal-comp [y-off]
-  (Rectangle. 3170 y-off 600 row-height))
+  (Rectangle. 1585 y-off 300 row-height))
 
 (defn reportable-external-comp [y-off]
-  (Rectangle. 3780 y-off 600 row-height))
+  (Rectangle. 1890 y-off 300 row-height))
 
 (defn other-comp [y-off]
-  (Rectangle. 4400 y-off 600 row-height))
+  (Rectangle. 2200 y-off 300 row-height))
 
 (defn parse-name-and-title [name-str]
   (when-let [[_ _ name title] (re-matches #"(\(\d+\))?\W*(.*)\W*(.*)\W*" name-str)]
@@ -47,6 +50,10 @@
 (defn parse-int [s]
   (.parse (NumberFormat/getNumberInstance java.util.Locale/US) s))
 
+(def number-chars "0123456789,.")
+
+(def name-chars "")
+
 (defn parse-comp [comp-str]
   (some->> comp-str
            (re-find #"[0123456789,]+")
@@ -56,9 +63,11 @@
   (let [file (if (on-page-one? idx) page-one page-two)
         tess (Tesseract/getInstance)
         y-off (y-offset idx)
-        doocr (fn [r] (.doOCR tess file r))]
-    (.setTessVariable tess "debug_file" "/dev/null")
-    (assoc (parse-name-and-title (doocr (name-rect y-off)))
+        doocr (fn [r] (.doOCR tess file r))
+        _ (.setTessVariable tess "tessedit_char_whitelist" name-chars)
+        name-and-title (-> y-off name-rect doocr parse-name-and-title)]
+    (.setTessVariable tess "tessedit_char_whitelist" number-chars)
+    (assoc name-and-title
       :hours (parse-hours (doocr (hours-rect y-off)))
       :reportable-internal-comp (parse-comp (doocr (reportable-internal-comp y-off)))
       :reportable-external-comp (parse-comp (doocr (reportable-external-comp y-off)))
@@ -78,7 +87,7 @@
   (let [doc (doto (PDFDocument.)
               (.load pdf))
         renderer (doto (SimpleRenderer.)
-                   (.setResolution 600))
+                   (.setResolution 300))
         [page-one page-two] (.render renderer doc 7 8)
         file-one (File. "one.png")
         file-two (File. "two.png")]
@@ -90,6 +99,7 @@
                      (read-record file-one file-two idx)
                      (catch Exception e
                        (println "Failed to read record" idx "from" (.getName pdf))
+                       (println "Exception:" (.getMessage e))
                        nil))]
         (if (empty? (:name record))
           records
@@ -114,6 +124,7 @@
   (println " DONE"))
 
 (defn -main [path out-path]
+  (.setTessVariable (Tesseract/getInstance) "debug_file" "/dev/null")
   (let [f (File. path)]
     (if (.isDirectory f)
       (doseq [pdf (.listFiles f)]
