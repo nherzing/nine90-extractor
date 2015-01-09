9057b81911f58210716c4982ef2b4e58c3a502a9
Nathan Herzing <nherzing@gmail.com>
2015-01-09 12:25:56 -0500

    Allow opening whitespace on name-and-title

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject tax-extractor "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :main tax-extractor.core
+  :main nine90-extractor.core
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
